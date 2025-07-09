bfe547bfc3eb5b9be56eef4a2bd76ba61ef2ad0a
g <diegocaucota@gmail.com>
2025-07-08 23:57:24 -0500

    fix: ListaNoticias row-cols

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -4,7 +4,7 @@ const ListaNoticias = ({ noticiaProps }) => {
   return (
     <div>
     <div className="text-center bg-white rounded-3 p-3">
-        <div className="row row-cols-1 row-cols-md-3 g-3">
+        <div className="row row-cols-1 row-cols-md-2 g-3">
           {noticiaProps.map((item, indice) => (
             <div className="col" key={indice}>
               <Noticia noticiaProps={item} />
