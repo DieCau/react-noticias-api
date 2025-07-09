eafaac697115b97f9ecd71bde6fe90ca96b006ef
g <diegocaucota@gmail.com>
2025-07-09 02:20:17 -0300

    fix: components ListaNoticias documentation

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -2,8 +2,8 @@ import Noticia from "./Noticia";
 
 const ListaNoticias = ({ noticiaProps }) => {
   return (
-    <div>
-    <div className="text-center bg-white rounded-3 p-3">
+    <>
+      <div className="text-center bg-white rounded-3 p-3">
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
           {noticiaProps.map((item, indice) => (
             <div className="col" key={indice}>
@@ -12,7 +12,7 @@ const ListaNoticias = ({ noticiaProps }) => {
           ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
